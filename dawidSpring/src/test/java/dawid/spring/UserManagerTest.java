47e61ecd41b0a4da9bc6c42ae49df33546074685
fiboll <fiboll@o2.pl>
2017-12-19 17:18:26 +0100

    add tests

--- dawidSpring/src/test/java/dawid/spring/UserManagerTest.java
+++ dawidSpring/src/test/java/dawid/spring/UserManagerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:context_test.xml")
@@ -31,6 +32,16 @@ public class UserManagerTest {
         users.forEach(
                 (User u) -> Assert.assertTrue(null != u.getId())
         );
+    }
+
+    @Test
+    public void testUserByNick() {
+        Optional<User> user = userManager.findUserByNick("fiboll");
+        Assert.assertNotNull(user);
+        Assert.assertEquals(user.get().getFirstName(), "Dawid");
+        Assert.assertEquals(user.get().getSecondName(), "Strembicki");
+        Assert.assertEquals(user.get().getNickname(), "fiboll");
+        Assert.assertEquals(user.get().getTable().getTitle(), "dawid table");
     }
 
 }
