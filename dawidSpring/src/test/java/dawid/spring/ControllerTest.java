742713356c9f8ce4add1ef4760a255516d0c2156
fiboll <fiboll@o2.pl>
2018-01-04 21:18:49 +0100

    add mvc test

--- dawidSpring/src/test/java/dawid/spring/ControllerTest.java
+++ dawidSpring/src/test/java/dawid/spring/ControllerTest.java
@@ -6,16 +6,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by dawid on 09.06.17.
@@ -29,6 +34,8 @@ import org.springframework.web.context.WebApplicationContext;
             "classpath:**/context_test.xml"
     }
 )
+@Transactional
+@Rollback
 public class ControllerTest {
 
     @Autowired
@@ -57,7 +64,7 @@ public class ControllerTest {
     }
 
     @Test
-    public void getNotExistUser() throws Exception{
+    public void getNotExistUserTest() throws Exception{
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
                 .get("/user?nick=nonexist");
         mockMvc.perform(builder)
@@ -66,11 +73,25 @@ public class ControllerTest {
     }
 
     @Test
-    public void getUser() throws Exception{
+    public void getUserTest() throws Exception{
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
                 .get("/user?nick=fiboll");
         mockMvc.perform(builder)
                .andExpect(MockMvcResultMatchers.status().isOk())
                .andExpect(MockMvcResultMatchers.view().name("userDetails"));
+    }
+
+    @Test
+    public void addTaskToNoneExistUser() throws Exception {
+
+        RequestBuilder builder = MockMvcRequestBuilders.post("/addTask")
+            .param("name", "test")
+            .param("desc", "testDesc")
+            .param("userNick", "fiboll23");
+
+
+        mockMvc.perform(builder)
+               .andDo(MockMvcResultHandlers.print())
+               .andExpect(MockMvcResultMatchers.view().name("redirect:noUser"));
     }
 }
