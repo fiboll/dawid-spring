0974ac69d489bb61229fdc875a86d9069b918f9e
fiboll <fiboll@o2.pl>
2018-03-10 12:57:13 +0100

    add validation

--- dawidSpring/src/main/java/dawid/spring/controller/TaskController.java
+++ dawidSpring/src/main/java/dawid/spring/controller/TaskController.java
@@ -3,9 +3,7 @@ package dawid.spring.controller;
 import dawid.spring.manager.TaskManager;
 import dawid.spring.model.dto.TaskDTO;
 import dawid.spring.model.entity.Label;
-import dawid.spring.model.entity.Task;
 import dawid.spring.provider.LabelDao;
-import dawid.spring.provider.TaskDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -43,9 +42,13 @@ public class TaskController {
     }
 
     @RequestMapping(value = "/editTask", method = RequestMethod.POST)
-    public String editTask(@ModelAttribute(value = "task") TaskDTO taskDTO,
+    public String editTask(@Valid @ModelAttribute(value = "task") TaskDTO taskDTO,
                            final BindingResult bindingResult,
                            Model model) {
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("task", taskDTO);
+            return "editForm";
+        }
 
         taskDTO = taskManager.updateTask(taskDTO);
         return "redirect:user?nick=" + taskDTO.getUserName();
@@ -53,8 +56,7 @@ public class TaskController {
 
     @RequestMapping(value = "/deleteTask", method = RequestMethod.GET)
     public String deleteTask(@RequestParam(value = "taskId") Long taskId,
-                             @RequestParam(value = "userName") String userName,
-                             Model model) {
+                             @RequestParam(value = "userName") String userName) {
 
         taskManager.deleteTask(taskId);
         return "redirect:user?nick=" + userName;
