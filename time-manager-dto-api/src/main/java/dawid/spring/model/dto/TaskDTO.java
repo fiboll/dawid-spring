e4a5f08f2b7f858f5dd00a72d78c2101e071d7fd
fiboll <fiboll@o2.pl>
2018-10-23 15:17:47 +0200

    edit form fixes

--- time-manager-dto-api/src/main/java/dawid/spring/model/dto/TaskDTO.java
+++ time-manager-dto-api/src/main/java/dawid/spring/model/dto/TaskDTO.java
@@ -1,17 +1,13 @@
 package dawid.spring.model.dto;
 
 import dawid.spring.comparator.TaskComparator;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.immutables.value.Value;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.util.*;
+import java.util.Date;
+import java.util.Set;
 
 @Value.Immutable
 @Value.Modifiable
@@ -30,12 +26,8 @@ public abstract class TaskDTO implements Comparable<TaskDTO> {
     public abstract boolean isDone();
     public abstract String getUserName();
 
-    @NotEmpty
+    @org.hibernate.validator.constraints.NotEmpty
     public abstract Set<LabelDTO> getLabels();
-
-    public void doneTask() {
-        //setDone(true);
-    }
 
     @Override
     public int compareTo(TaskDTO other) {
