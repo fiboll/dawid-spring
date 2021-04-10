16b218557bed8d71e46bcae37a0a976e9df331d1
fiboll <fiboll@o2.pl>
2021-04-10 12:46:23 +0200

    update module info

--- time-manager-dao-api/src/main/java/module-info.java
+++ time-manager-dao-api/src/main/java/module-info.java
@@ -2,6 +2,6 @@
  * @author Dawid Strembicki Hycom (dawid.strembicki.hycom@hycom.pl)
  */
 module time.manager.dao.api {
-    requires static time.manager.model;
+    requires transitive time.manager.model;
     exports dawid.spring.provider;
 }
